d3625a5fdb0866fc093b36d17cbc1a6e2604df9f
«iliya <iliya.91f@gmail.com>
2023-04-23 18:04:22 +0300

    added comments

--- cmake-build-debug/CMakeFiles/Binary_tree.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Binary_tree.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/iliyailchishin/Desktop/GB/C++/Binary tree/main.cpp" "/Users/iliyailchishin/Desktop/GB/C++/Binary tree/cmake-build-debug/CMakeFiles/Binary_tree.dir/main.cpp.o"
+  "/Users/iliyailchishin/Desktop/GB/C++/tree/main.cpp" "/Users/iliyailchishin/Desktop/GB/C++/tree/cmake-build-debug/CMakeFiles/Binary_tree.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ private:
     enum class ColorNode {
         RED, BLACK
     };
+    // определяем узел дерева
     struct Node {
         Node* left;
         Node* right;
@@ -17,7 +18,7 @@ private:
     };
 
     Node* root;
-
+//  поворот узла влево
     Node* rotateLeft(Node* myNode) {
         Node* child = myNode->right;
         Node* childLeft = child->left;
@@ -25,7 +26,7 @@ private:
         myNode->right = childLeft;
         return child;
     }
-
+//  поворот узла влево
     Node* rotateRight(Node* myNode) {
         Node* child = myNode->left;
         Node* childRight = child->right;
@@ -33,7 +34,7 @@ private:
         myNode->left = childRight;
         return child;
     }
-
+// если узел красный, возвращаем true
     bool isRed(Node* myNode) {
         return myNode != nullptr && myNode->color == ColorNode::RED;
     }
@@ -43,39 +44,48 @@ private:
         node1->color = node2->color;
         node2->color = temp;
     }
-
+// Рекурсивный метод вставки нового узла в дерево
     Node* insert(Node* myNode, V data) {
+        // Если узел пуст, создаем новый узел с данными data и цветом RED
         if (myNode == nullptr) {
             Node* newNode = new Node(data);
             newNode->color = ColorNode::RED;
             return newNode;
         }
 
+        // Если значение data меньше, чем значение данных в текущем узле, идем влево
         if (myNode->data > data) {
             myNode->left = insert(myNode->left, data);
+            // Если значение data больше, чем значение данных в текущем узле, идем вправо
         } else if (myNode->data < data) {
             myNode->right = insert(myNode->right, data);
+            // Если значение data равно значению данных в текущем узле, возвращаем текущий узел
         } else {
             return myNode;
         }
 
+        // делаем балансировку
         myNode = balanced(myNode);
 
         return myNode;
     }
+// Балансировка дерева
 
     Node* balanced(Node* myNode) {
+        // Красный узел является правым потомком, а его левый потомок черный.
         if (isRed(myNode->right) && !isRed(myNode->left)) {
             myNode = rotateLeft(myNode);
             swapColors(myNode, myNode->left);
         }
 
         if (isRed(myNode->left) && isRed(myNode->left->left)) {
+            // Красный узел является левым потомком, а его левый потомок тоже красный.
             myNode = rotateRight(myNode);
             swapColors(myNode, myNode->right);
         }
 
         if (isRed(myNode->left) && isRed(myNode->right)) {
+            // Красный узел имеет двух красных потомков.
             myNode->color = ColorNode::RED;
             myNode->left->color = ColorNode::BLACK;
             myNode->right->color = ColorNode::BLACK;
@@ -85,6 +95,7 @@ private:
     }
 
     void inorder(Node* node) {
+        // Рекурсивная функция для обхода дерева в порядке возрастания и печати значений узлов вместе с их цветом.
         if (node != nullptr) {
             inorder(node->left);
             std::cout << node->data << " " << (node->color == ColorNode::RED ? "RED" : "BLACK") << "|";
@@ -92,6 +103,7 @@ private:
         }
     }
 public:
+    // Конструктор класса. Устанавливает корень в nullptr.
     RedBlackTree() : root(nullptr) {}
     bool insert(V data) {
         Node *node;
@@ -112,7 +124,7 @@ public:
         return true;
     }
 
-
+// Публичная функция для вызова обхода дерева в порядке возрастания и печати значений узлов вместе с их цветом.
     void inorder() {
                 inorder(root);
             }
@@ -123,9 +135,14 @@ public:
 
 int main() {
     RedBlackTree<int> tree;
-    tree.insert(1);
-    tree.insert(52);
-    std::cout<< tree.insert(9) << endl;
+    tree.insert(15);
     tree.insert(11);
+    tree.insert(32);
+    tree.insert(3);
+    tree.insert(9);
+    tree.insert(25233);
+    tree.inorder();
 
+
+    return 0;
 }
